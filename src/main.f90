1f409388e56d9a47467386759debda038331716b
Saeed Sarpas <saeed.sarpas@phys.ethz.ch>
2018-09-03 16:03:55 +0200

    Roadmap - Writing down the steps

--- src/main.f90
+++ src/main.f90
@@ -1,4 +1,50 @@
 program rhyme
   implicit none
 
+  ! Initialize date and time
+
+  ! Reading parameter file and converting to code units
+
+  ! Initialize cosmological variables (if COSMO is set)
+
+  ! Setup patch-based GRID
+
+  ! Creating emission spectra for ionizing sources
+
+  ! Initialize ionization cross section
+
+  ! Initialize coefficients
+
+  ! Initialize pre-existing HII regions
+
+  ! Drop a snapshot (if needed)
+
+  ! Create problem domain array (if needed)
+
+  ! Time evolution loop
+
+  !! Compute Timestep
+
+  !! if HYDRO is set
+  !!! Copy old hydro states to new ones
+  !!! Hydro loop
+  !!!! Setup boundary conditions
+  !!!! Gather neighboring cells (into a 6x6x6 array)
+  !!!! Calculating slope limiter
+  !!!! Extrapolate states to cell faces
+  !!!! Riemann solver
+  !!!! Sampling solution
+  !!!! Updating fluxes
+  !!! end Hydro loop
+  !!! Rewrite new hydro states into old ones
+  !! end if Hydro is set
+
+  !! Get active sources
+
+  !! Deoposite photons loop
+  !!! Ray tracing
+  !!! Solving the ionization equations
+  !! end Deposite photons loop
+
+  !! Drop snapshot (if needed)
 end program rhyme
