df509676dfde275076e8b17429eb9a6bc51e2a88
Saeed Sarpas <saeed.sarpas@phys.ethz.ch>
2018-11-23 20:58:06 +0100

    [WIP] Param parser -

--- src/param_parser_module.f90
+++ src/param_parser_module.f90
@@ -2,4 +2,20 @@ module param_parser_module
   use rhyme_nombre
 
   implicit none
+
+contains
+
+  logical function parse_params (param_file) result (passed)
+    implicit none
+
+    character(len=1024), intent(in) :: param_file
+
+
+    open (1, file=param_file, action='read', form="formatted")
+
+    do
+    end do
+
+    close (1)
+  end function parse_params
 end module param_parser_module
