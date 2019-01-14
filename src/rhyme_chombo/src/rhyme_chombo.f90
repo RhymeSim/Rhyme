187944622533cbeff895ccba991211ec67691343
Saeed Sarpas <saeed.sarpas@phys.ethz.ch>
2019-01-14 21:58:34 +0100

    Added add_comp_array_attr procedure to chombo object

--- src/rhyme_chombo/src/rhyme_chombo.f90
+++ src/rhyme_chombo/src/rhyme_chombo.f90
@@ -21,6 +21,7 @@ module rhyme_chombo
     procedure :: setup => rhyme_chombo_setup
     procedure :: add_attr => rhyme_chombo_add_attribute
     procedure :: add_array_attr => rhyme_chombo_add_array_attribute
+    procedure :: add_comp_array_attr => rhyme_chombo_add_compound_array_attribute
     procedure :: close => rhyme_chombo_close
   end type rhyme_chombo_t
 
@@ -56,7 +57,6 @@ contains
     this%initialized = .true.
 
     n_dims = size ( samr%base_grid ) - sum ( samr%base_grid * merge ( 1, 0, samr%base_grid <= 1 ) )
-    print *, samr%base_grid
 
     call h5gcreate_f ( this%fid, "/chombo_global", group_id, hdferr )
     call this%add_attr ( "/chombo_global", "SpaceDim", n_dims )
@@ -109,6 +109,8 @@ contains
       call h5aclose_f ( attr_id, hdferr )
 
     end select
+
+    call h5gclose_f ( group_id, hdferr )
   end subroutine rhyme_chombo_add_attribute
 
 
@@ -123,6 +125,8 @@ contains
 
     integer ( hid_t ) :: group_id, space_id, attr_id
     integer :: hdferr
+
+    if ( .not. this%initialized ) return
 
     call h5gopen_f ( this%fid, trim(where), group_id, hdferr )
 
@@ -143,7 +147,72 @@ contains
 
     end select
 
+    call h5gclose_f ( group_id, hdferr )
   end subroutine rhyme_chombo_add_array_attribute
+
+
+  subroutine rhyme_chombo_add_compound_array_attribute ( this, where, name, keys, values )
+    implicit none
+
+    class ( rhyme_chombo_t ), intent(inout) :: this
+    character(len=*), intent(in) :: where, name
+    character(len=*), dimension(:), intent(in) :: keys
+    class (*), dimension(:) :: values
+
+    integer ( hid_t ) :: group_id
+    integer :: hdferr
+
+    if ( .not. this%initialized ) return
+
+    call h5gopen_f ( this%fid, trim(where), group_id, hdferr )
+
+    select type ( vals => values)
+    type is ( integer )
+      call write_comp_array_attr ( H5T_NATIVE_INTEGER )
+    type is ( real ( kind = 4 ) )
+      call write_comp_array_attr ( H5T_NATIVE_REAL )
+    type is ( real ( kind = 8 ) )
+      call write_comp_array_attr ( H5T_NATIVE_DOUBLE )
+    end select
+
+    call h5gclose_f ( group_id, hdferr )
+
+  contains
+    subroutine write_comp_array_attr ( type_id )
+      implicit none
+
+      integer ( hid_t ) :: type_id
+
+      integer ( hid_t ) :: space_id, attr_id, comp_type
+      integer ( size_t ) :: element_size, tot_size, offset
+      integer ( hsize_t ) :: dims(1) = 1
+      integer :: i
+
+      call h5tget_size_f ( type_id, element_size, hdferr )
+      tot_size = size ( keys ) * element_size
+      call h5tcreate_f ( H5T_COMPOUND_F , tot_size, comp_type, hdferr )
+
+      offset = 0
+      do i = 1, size ( keys )
+        call h5tinsert_f ( comp_type, trim ( keys(i) ), offset, type_id, hdferr )
+        offset = offset + element_size
+      end do
+
+      call h5screate_simple_f ( 1, dims, space_id, hdferr )
+      call h5acreate_f ( group_id, trim(name), comp_type, space_id, attr_id, hdferr )
+
+      select type ( vals => values)
+      type is ( integer )
+        call h5awrite_f ( attr_id, comp_type, vals, dims, hdferr)
+      type is ( real ( kind = 4 ))
+        call h5awrite_f ( attr_id, comp_type, vals, dims, hdferr)
+      type is ( real ( kind = 8 ))
+        call h5awrite_f ( attr_id, comp_type, vals, dims, hdferr)
+      end select
+
+      call h5aclose_f ( attr_id, hdferr )
+    end subroutine write_comp_array_attr
+  end subroutine rhyme_chombo_add_compound_array_attribute
 
 
   subroutine rhyme_chombo_close ( this )
@@ -153,7 +222,7 @@ contains
 
     integer :: hdferr
 
-    if ( this%initialized ) return
+    if ( .not. this%initialized ) return
 
     call h5fclose_f ( this%fid, hdferr )
     ! TODO: check hdferr
