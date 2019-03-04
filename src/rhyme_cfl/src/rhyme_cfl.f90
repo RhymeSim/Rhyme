71d2050ed3a98edfee41bf3179343081b3d2ffac
Saeed Sarpas <saeed.sarpas@phys.ethz.ch>
2019-03-04 15:29:39 +0100

    [Bug fixed] in CFL dt calculation - Updating dt for zeroth level

--- src/rhyme_cfl/src/rhyme_cfl.f90
+++ src/rhyme_cfl/src/rhyme_cfl.f90
@@ -21,38 +21,28 @@ contains
     type ( ideal_gas_t ), intent(in) :: ig
     type ( samr_t ), intent(in) :: samr
 
-    integer :: l, b
+    integer :: level, l, b, i, j, k
     real(kind=8) :: max_cs
 
+    level = 0
     max_cs = ig%cs ( samr%levels(0)%boxes(1)%hydro(1,1,1) )
 
     do l = 0, samr%nlevels - 1
       do b = 1, samr%levels(l)%nboxes
-        call find_max_cs_in_a_box ( samr%levels(l)%boxes(b), max_cs )
-        max_cs = max_cs * 2.d0**l ! Updating dt for the base level
-      end do
-    end do
-
-    dt = this%courant_number * minval ( samr%levels(0)%dx ) / max_cs
-
-  contains
-    pure subroutine find_max_cs_in_a_box ( box, cs )
-      implicit none
-
-      type ( samr_box_t ), intent(in) :: box
-      real(kind=8), intent(inout) :: cs
-
-      integer :: i, j, k
-
-      do k = 1, box%dims(3)
-        do j = 1, box%dims(2)
-          do i = 1, box%dims(1)
-            if ( ig%cs ( box%hydro(i, j, k) ) > cs ) then
-              cs = ig%cs ( box%hydro(i, j, k) )
-            end if
+        do k = 1, samr%levels(l)%boxes(b)%dims(3)
+          do j = 1, samr%levels(l)%boxes(b)%dims(2)
+            do i = 1, samr%levels(l)%boxes(b)%dims(1)
+              if ( ig%cs( samr%levels(l)%boxes(b)%hydro(i, j, k) ) > max_cs ) then
+                max_cs = ig%cs ( samr%levels(l)%boxes(b)%hydro(i, j, k) )
+                level = l
+              end if
+            end do
           end do
         end do
       end do
-    end subroutine find_max_cs_in_a_box
+    end do
+
+    max_cs = max_cs * 2.d0**level ! Updating dt for the base level
+    dt = this%courant_number * minval ( samr%levels(0)%dx ) / max_cs
   end function calculate_time_step
 end module rhyme_cfl
