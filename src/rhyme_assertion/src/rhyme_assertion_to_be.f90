447a37cbf9295d0b3221f8e42c232b10de56313a
saeedSarpas <s.sarpas@gmail.com>
2019-04-08 01:17:36 +0200

    [WIP] Assertion - Setting val, op and exp in to_be

--- src/rhyme_assertion/src/rhyme_assertion_to_be.f90
+++ src/rhyme_assertion/src/rhyme_assertion_to_be.f90
@@ -9,17 +9,68 @@ contains
     logical :: passed
     passed = .false.
 
+    test%op = 'to_be'
+
     select type ( v => val )
     type is ( integer )
       test%type = assertid%int
+      write ( test%val, assertcnst%int_fmt ) v
 
       select type ( e => exp )
       type is ( integer )
+        write ( test%exp, assertcnst%int_fmt ) e
         passed = v .eq. e
       class default
         passed = .false.
       end select
 
+    type is ( real( kind=4 ) )
+      test%type = assertid%real
+      write ( test%val, assertcnst%real_fmt ) v
+
+      select type ( e => exp )
+      type is ( real( kind=4 ) )
+        passed = abs( v - e ) < epsilon(0.e0)
+      class default
+        passed = .false.
+      end select
+
+    type is ( real( kind=8 ) )
+      test%type = assertid%double
+      write ( test%val, assertcnst%double_fmt ) v
+
+      select type ( e => exp )
+      type is ( real( kind=8 ) )
+        passed = abs( v - e ) < epsilon(0.d0)
+      class default
+        passed = .false.
+      end select
+
+    type is ( character(*) )
+      test%type = assertid%char
+      test%val = trim( adjustl( v ) )
+
+      select type ( e => exp )
+      type is ( character(*) )
+        passed = trim( adjustl( v ) ) .eq. trim( adjustl( e ) )
+      class default
+        passed = .false.
+      end select
+
+    end select
+
+
+    select type ( e => exp )
+    type is ( integer )
+      write ( test%exp, assertcnst%int_fmt ) e
+    type is ( real( kind=4 ) )
+      write ( test%exp, assertcnst%real_fmt ) e
+    type is ( real( kind=8 ) )
+      write ( test%exp, assertcnst%double_fmt ) e
+    type is ( character(*) )
+      test%exp = trim( adjustl( e ) )
+    class default
+      test%exp = 'Unsupported type'
     end select
 
     test%is_passed = passed
--- src/rhyme_assertion/tests/rhyme_assertion_to_be_test.f90
+++ src/rhyme_assertion/tests/rhyme_assertion_to_be_test.f90
@@ -4,14 +4,63 @@ logical function rhyme_assertion_to_be_test () result ( failed )
   implicit none
 
   integer, parameter :: int_value = 123
+  real( kind=4 ), parameter :: real_value = 1.23e4
+  real( kind=8 ), parameter :: double_value = 12.3d4
+  character ( len=8 ), parameter :: char_value = 'string'
 
-  type ( test_t ) :: test
+  character ( len=128 ) :: int_str, real_str, double_str, char_str
+
+  type ( test_t ) :: test_i, test_r, test_d, test_c
   integer :: int_test
+  real( kind=4 ) :: real_test
+  real ( kind=8 ) :: double_test
+  character ( len=8 ) :: char_test
 
   int_test = int_value
-  test = int_test .toBe. int_value
+  test_i = int_test .toBe. int_value
+  write ( int_str, assertcnst%int_fmt ) int_value
 
   failed = &
-  test%is_passed .neqv. .true. &
-  .or. test%type .ne. assertid%int
+  .not. test_i%is_passed &
+  .or. test_i%type .ne. assertid%int &
+  .or. test_i%val .ne. int_str &
+  .or. test_i%op .ne. 'to_be' &
+  .or. test_i%exp .ne. int_str
+  if ( failed ) return
+
+  real_test = real_value
+  test_r = real_test .toBe. real_value
+  write ( real_str, assertcnst%real_fmt ) real_value
+
+  failed = &
+  .not. test_r%is_passed &
+  .or. test_r%type .ne. assertid%real &
+  .or. test_r%val .ne. real_str &
+  .or. test_r%op .ne. 'to_be' &
+  .or. test_r%exp .ne. real_str
+  if ( failed ) return
+
+  double_test = double_value
+  test_d = double_test .toBe. double_value
+  write ( double_str, assertcnst%double_fmt ) double_value
+
+  failed = &
+  .not. test_d%is_passed &
+  .or. test_d%type .ne. assertid%double &
+  .or. test_d%val .ne. double_str &
+  .or. test_d%op .ne. 'to_be' &
+  .or. test_d%exp .ne. double_str
+  if ( failed ) return
+
+  char_test = char_value
+  test_c = char_test .toBe. char_value
+  char_str = char_value
+
+  failed = &
+  .not. test_c%is_passed &
+  .or. test_c%type .ne. assertid%char &
+  .or. test_c%val .ne. char_str &
+  .or. test_c%op .ne. 'to_be' &
+  .or. test_c%exp .ne. char_str
+  if ( failed ) return
 end function rhyme_assertion_to_be_test
