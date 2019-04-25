0518188e14435a9c4fbfcd61a699272f1d30c472
Saeed Sarpas <saeed.sarpas@phys.ethz.ch>
2019-04-25 11:34:55 +0200

    [Done] Using assertion - rhyme_slope_limiter

--- src/rhyme_slope_limiter/tests/rhyme_slope_limiter_superbee_test.f90
+++ src/rhyme_slope_limiter/tests/rhyme_slope_limiter_superbee_test.f90
@@ -1,12 +1,17 @@
 logical function rhyme_slope_limiter_superbee_test () result (failed)
   use rhyme_slope_limiter_factory
+  use rhyme_assertion
 
   implicit none
+
+  type ( assertion_t ) :: sl_tester
 
   type ( slope_limiter_t ) :: sl
   type ( hydro_conserved_t ) :: Delta
   type ( cfl_t ) :: cfl
   type ( ideal_gas_t ) :: ig
+
+  sl_tester = .describe. "slope_limiter_superbee"
 
   call rhyme_slope_limiter_factory_init
   call ig%init_with( chemi, thermo, igid%diatomic, log )
@@ -17,30 +22,22 @@ logical function rhyme_slope_limiter_superbee_test () result (failed)
 
   UM%u = hy%cons%u * 1.23d0
   call sl%superbee( cfl, ig, UL, UM, UR, Delta )
-
-  failed = any ( abs ( Delta%u ) > epsilon(0.d0) )
-  if ( failed ) return
-
+  call sl_tester%expect( Delta%u .toBe. 0.d0 )
 
   UM%u = hy%cons%u * (-2.34)
   call sl%superbee( cfl, ig, UL, UM, UR, Delta )
-
-  failed = any ( abs ( Delta%u ) > epsilon(0.d0) )
-  if ( failed ) return
+  call sl_tester%expect( Delta%u .toBe. 0.d0 )
 
 
   UM%u = hy%cons%u * 1.23d0
   UR%u = hy%cons%u * 2.34d0
   call sl%superbee( cfl, ig, UL, UM, UR, Delta )
-
-  failed = any ( abs ( Delta%u ) < epsilon(0.d0) )
-  if ( failed ) return
-
+  call sl_tester%expect( Delta%u .notToBe. 0.d0 )
 
   UM%u = hy%cons%u * (-1.23d0)
   UR%u = hy%cons%u * (-2.34d0)
   call sl%superbee( cfl, ig, UL, UM, UR, Delta )
+  call sl_tester%expect( Delta%u .notToBe. 0.d0 )
 
-  failed = any ( abs ( Delta%u ) < epsilon(0.d0) )
-  if ( failed ) return
+  failed = sl_tester%failed()
 end function rhyme_slope_limiter_superbee_test
--- src/rhyme_slope_limiter/tests/rhyme_slope_limiter_test.f90
+++ src/rhyme_slope_limiter/tests/rhyme_slope_limiter_test.f90
@@ -1,19 +1,22 @@
 logical function rhyme_slope_limiter_test () result ( failed )
   use rhyme_slope_limiter
+  use rhyme_assertion
 
   implicit none
 
+  type ( assertion_t ) :: sl_tester
+
   type ( slope_limiter_t ) :: sl
 
-  failed = &
-  slid%van_Leer .ne. 1 &
-  .or. slid%minmod .ne. 2 &
-  .or. slid%van_albada .ne. 3 &
-  .or. slid%superbee .ne. 4
+  sl_tester = .describe. "slope_limiter"
 
-  if ( failed ) return
+  call sl_tester%expect( slid%van_Leer .toBe. 1 )
+  call sl_tester%expect( slid%minmod .toBe. 2 )
+  call sl_tester%expect( slid%van_albada .toBe. 3 )
+  call sl_tester%expect( slid%superbee .toBe. 4 )
+  
+  call sl_tester%expect( sl%w .toBe. 0.d0 )
+  call sl_tester%expect( sl%type .toBe. slid%minmod )
 
-  failed = &
-  abs ( sl%w ) > epsilon(0.d0) &
-  .or. sl%type .ne. slid%minmod
+  failed = sl_tester%failed()
 end function rhyme_slope_limiter_test
