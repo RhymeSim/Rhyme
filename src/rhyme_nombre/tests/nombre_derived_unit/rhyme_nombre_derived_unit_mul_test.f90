16c94cbd4ef90d8d82cab988aca37d54072359c2
saeedSarpas <s.sarpas@gmail.com>
2019-09-19 04:18:25 +0430

    [WIP] spectrum - wip: refactoring nombre module derived_unit mul

--- src/rhyme_nombre/tests/nombre_derived_unit/rhyme_nombre_derived_unit_mul_test.f90
+++ src/rhyme_nombre/tests/nombre_derived_unit/rhyme_nombre_derived_unit_mul_test.f90
@@ -1,5 +1,6 @@
 logical function rhyme_nombre_derived_unit_mul_test () result ( failed )
   use rhyme_nombre_derived_unit_factory
+  use rhyme_nombre_base_unit_factory
   use rhyme_assertion
 
   implicit none
@@ -11,42 +12,41 @@ logical function rhyme_nombre_derived_unit_mul_test () result ( failed )
   type ( nombre_base_unit_t ) :: bu(3)
 
   real ( kind=8 ) :: rnd(3)
-  integer :: idx(3) , i
+  integer :: i
 
   tester = .describe. "nombre_derived_unit_mul"
 
   do i = 1, 5
     call random_number( rnd )
 
-    idx = int( rnd ) * size( si_base_units ) + 1
-    bu = si_base_units( idx )
-
+    bu = si_base_units( ceiling( rnd * size( si_base_units ) ) )
     buc => nom_du_factory%generate_chain( [ bu(1) ] )
-    iduc => int( rnd(1) ) * buc
+
+    iduc => int( rnd(1) * 10 ) * buc
     call tester%expect( associated( iduc%prev ) .toBe. .false. .hint. 'left end' )
-    call tester%expect( iduc%conv .toBe. int( rnd(1) ) )
-    call tester%expect( iduc%head == buc .toBe. .true. )
+    call tester%expect( iduc%conv .toBe. int( rnd(1) * 10 ) )
+    call tester%expect( iduc%head .toBe. buc )
     call tester%expect( associated( iduc%head%next ) .toBe. .false. )
-    call tester%expect( associated( iduc%next%next ) .toBe. .false. .hint. 'right end' )
+    call tester%expect( associated( iduc%next ) .toBe. .false. .hint. 'right end' )
 
     buc => nom_du_factory%generate_chain( bu(1:2) )
     rduc => real( rnd(2), kind=4 ) * buc
     call tester%expect( associated( rduc%prev ) .toBe. .false. .hint. 'left end' )
     call tester%expect( rduc%conv .toBe. real( rnd(2), kind=4 ) )
-    call tester%expect( rduc%head == buc .toBe. .true. )
-    call tester%expect( rduc%head%next == buc%next .toBe. .true. )
+    call tester%expect( rduc%head .toBe. buc )
+    call tester%expect( rduc%head%next .toBe. buc%next )
     call tester%expect( associated( rduc%head%next%next ) .toBe. .false. )
-    call tester%expect( associated( rduc%next%next ) .toBe. .false. .hint. 'right end' )
+    call tester%expect( associated( rduc%next ) .toBe. .false. .hint. 'right end' )
 
     buc => nom_du_factory%generate_chain( bu(1:3) )
     r8duc => rnd(3) * buc
     call tester%expect( associated( r8duc%prev ) .toBe. .false. .hint. 'left end' )
     call tester%expect( r8duc%conv .toBe. rnd(3) )
-    call tester%expect( r8duc%head == buc .toBe. .true. )
-    call tester%expect( r8duc%head%next == buc%next .toBe. .true. )
-    call tester%expect( r8duc%head%next%next == buc%next%next .toBe. .true. )
+    call tester%expect( r8duc%head .toBe. buc )
+    call tester%expect( r8duc%head%next .toBe. buc%next )
+    call tester%expect( r8duc%head%next%next .toBe. buc%next%next )
     call tester%expect( associated( r8duc%head%next%next%next ) .toBe. .false. )
-    call tester%expect( associated( r8duc%next%next ) .toBe. .false. .hint. 'right end' )
+    call tester%expect( associated( r8duc%next ) .toBe. .false. .hint. 'right end' )
   end do
 
   failed = tester%failed()
